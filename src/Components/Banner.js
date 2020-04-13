105993633ac19171f7f412b4a4027be20a8bd31b
Ämit Purohit <amit.purohit@moonraft.com>
2020-04-13 22:29:14 +0530

    testing purpose

--- src/Components/Banner.js
+++ src/Components/Banner.js
@@ -1,108 +1,35 @@
 import React, { Component } from 'react';
-import { Bann, BannInnerWrap, BannBtn, CaptionBann,BackDrop, bgImage } from './style';
-import linkBtn_0 from './Assets/images/sliderA_01.jpg';
+import { Bann, BannInnerWrap, BannBtn, CaptionBann, BackDrop, bgImage } from './style';
+import { listLinkBtn, captionData } from '../Components/Data';
 
-var listLinkBtn = [
-    {
-        "text1": "Mexican Grilled",
-        "text2": " Corn Recipe"
-    },
-    {
-        "text1": "Roast Chicken",
-        "text2": " With Lemon Gravy"
-    },
-    {
-        "text1": "Avocado Melon Salad ",
-        "text2": "With Lime Vinaigrette "
-    },
-    {
-        "text1": "Chunky Beef Stew",
-        "text2": ""
-    },
-    {
-        "text1": "Farmhouse Vegetable ",
-        "text2": "And Barley Soup"
-    },
-
-]
-var captionData = [{
-    "id":"linkBtn_0",
-    "badge":"Baking",
-    "heading":"Mexican Grilled Corn Recipe",
-    "serve":"4 Servings",
-    "time":"30 MIN",
-    "cheff":"By Sandra Fortin",
-    "img": require('./Assets/images/sliderA_01.jpg')
-},
-{
-    "id":"linkBtn_1",
-    "badge":"CURRY",
-    "heading":"Roast Chicken With Lemon Gravy",
-    "serve":"4 Servings",
-    "time":"1 HR 20 MIN",
-    "cheff":"BY SANDRA FORTIN",
-    "img": require('./Assets/images/sliderA_02.jpg')
-
-},
-{
-    "id":"linkBtn_2",
-    "badge":"SALADS",
-    "heading":"Avocado Melon Salad With Lime Vinaigrette",
-    "serve":"1 SERVINGS",
-    "time":"15 MIN",
-    "cheff":"BY SANDRA FORTIN",
-    "img": require('./Assets/images/sliderA_03.jpg')
-
-},
-{
-    "id":"linkBtn_3",
-    "badge":"BEEF",
-    "heading":"Chunky Beef Stew",
-    "serve":"4 Servings",
-    "time":"1 HR 20 MIN",
-    "cheff":"BY SANDRA FORTIN",
-    "img": require('./Assets/images/sliderA_04.jpg')
-
-},
-{
-    "id":"linkBtn_4",
-    "badge":"SOUPS",
-    "heading":"Farmhouse Vegetable And Barley Soup",
-    "serve":"4 Servings",
-    "time":"1 HR 30 MIN",
-    "cheff":"BY SANDRA FORTIN",
-    "img": require('./Assets/images/sliderA_04.jpg')
-
-}
-]
 class Banner extends Component {
-    constructor(props){  
-        super(props);  
-        this.state={
-            badge:"Baking",
-            heading:"Mexican Grilled Corn Recipe",
-            serve:"4 Servings",
-            time:"30 MIN",
-            cheff:"By Sandra Fortin",
+    constructor(props) {
+        super(props);
+        this.state = {
+            badge: "Baking",
+            heading: "Mexican Grilled Corn Recipe",
+            serve: "4 Servings",
+            time: "30 MIN",
+            cheff: "By Sandra Fortin",
             img: require('./Assets/images/sliderA_01.jpg')
         }
-      } 
-    switchContent=(idx)=>{
+    }
+    switchContent = (idx) => {
         for (var i = 0; i < captionData.length; i++) {
             if (captionData[i].id === idx) {
-                    
-                     this.setState({
-                         badge:captionData[i].badge,
-                         heading:captionData[i].heading,
-                         serve:captionData[i].serve,
-                         time:captionData[i].time,
-                         cheff:captionData[i].cheff,
-                         img: captionData[i].img
-                     })
+
+                this.setState({
+                    badge: captionData[i].badge,
+                    heading: captionData[i].heading,
+                    serve: captionData[i].serve,
+                    time: captionData[i].time,
+                    cheff: captionData[i].cheff,
+                    img: captionData[i].img
+                })
             }
         }
     }
-   
+
     render() {
         return (
             <Bann>
@@ -110,7 +37,7 @@ class Banner extends Component {
                     <BackDrop id="back off">
                         <CaptionBann>
                             <li>{this.state.badge}</li>
-                                    <h1><a href=""> {this.state.heading}</a></h1>
+                            <h1><a href=""> {this.state.heading}</a></h1>
                             <p>
                                 <span><i className="fa fa-cutlery" aria-hidden="true"></i> {this.state.serve}</span>
                                 <span><i className="fa fa-clock-o" aria-hidden="true"></i> {this.state.time}</span>
@@ -124,7 +51,7 @@ class Banner extends Component {
                     <nav>
                         <ul className="btnWrap">
                             {listLinkBtn.map((items, i) => {
-                                return <li onClick={()=>this.switchContent("linkBtn_"+i)} key={i} id={"linkBtn_"+i}><span>{items.text1}<br/>{items.text2}</span></li>
+                                return <li onClick={() => this.switchContent("linkBtn_" + i)} key={i} id={"linkBtn_" + i}><span>{items.text1}<br />{items.text2}</span></li>
                             })}
                         </ul>
                     </nav>
---
+++ src/Components/Data.js
@@ -0,0 +1,74 @@
+export const listLinkBtn = [
+    {
+        "text1": "Mexican Grilled",
+        "text2": " Corn Recipe"
+    },
+    {
+        "text1": "Roast Chicken",
+        "text2": " With Lemon Gravy"
+    },
+    {
+        "text1": "Avocado Melon Salad ",
+        "text2": "With Lime Vinaigrette "
+    },
+    {
+        "text1": "Chunky Beef Stew",
+        "text2": ""
+    },
+    {
+        "text1": "Farmhouse Vegetable ",
+        "text2": "And Barley Soup"
+    },
+
+]
+
+export const captionData = [{
+    "id":"linkBtn_0",
+    "badge":"Baking",
+    "heading":"Mexican Grilled Corn Recipe",
+    "serve":"4 Servings",
+    "time":"30 MIN",
+    "cheff":"By Sandra Fortin",
+    "img": require('./Assets/images/sliderA_01.jpg')
+},
+{
+    "id":"linkBtn_1",
+    "badge":"CURRY",
+    "heading":"Roast Chicken With Lemon Gravy",
+    "serve":"4 Servings",
+    "time":"1 HR 20 MIN",
+    "cheff":"BY SANDRA FORTIN",
+    "img": require('./Assets/images/sliderA_02.jpg')
+
+},
+{
+    "id":"linkBtn_2",
+    "badge":"SALADS",
+    "heading":"Avocado Melon Salad With Lime Vinaigrette",
+    "serve":"1 SERVINGS",
+    "time":"15 MIN",
+    "cheff":"BY SANDRA FORTIN",
+    "img": require('./Assets/images/sliderA_03.jpg')
+
+},
+{
+    "id":"linkBtn_3",
+    "badge":"BEEF",
+    "heading":"Chunky Beef Stew",
+    "serve":"4 Servings",
+    "time":"1 HR 20 MIN",
+    "cheff":"BY SANDRA FORTIN",
+    "img": require('./Assets/images/sliderA_04.jpg')
+
+},
+{
+    "id":"linkBtn_4",
+    "badge":"SOUPS",
+    "heading":"Farmhouse Vegetable And Barley Soup",
+    "serve":"4 Servings",
+    "time":"1 HR 30 MIN",
+    "cheff":"BY SANDRA FORTIN",
+    "img": require('./Assets/images/sliderA_04.jpg')
+
+}
+]
